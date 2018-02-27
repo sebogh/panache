700f9798fd34d13d1aaf20d8f207fb7c94fb127d
Sebastian Bogan <sebogh@qibli.net>
2018-02-27 11:16:18 +0100

    bumped version

--- src/panache.py
+++ src/panache.py
@@ -32,7 +32,7 @@ script_dir = os.path.dirname(script)
 script_base = os.path.basename(script)
 user_home = os.path.expanduser("~")
 default_style_dir = os.path.join(user_home, ".panache").replace(os.path.sep, '/')
-version = "0.2.3"
+version = "0.2.4"
 
 # setup logging
 logging.basicConfig(format="%(message)s")
